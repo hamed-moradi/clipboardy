8a7dfca32696186ff4a847787f3c15addcbc5f6c
AmirH6977 <107415904+AmirH6977@users.noreply.github.com>
2023-02-18 16:21:02 +0330

    feat(DashBoard):about add/get clipBoard

--- src/Presentation/Presentation.DashBoard/src/app/clipBoard/clipBoard-list/clipBoard-item/clipBoard-item.component.ts
+++ src/Presentation/Presentation.DashBoard/src/app/clipBoard/clipBoard-list/clipBoard-item/clipBoard-item.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { ColorUsedService } from 'src/app/help/color-used.service';
-import { ClipBoard } from '../../clipBoard.model';
+import { Component, OnInit, Input } from "@angular/core";
+import { ColorUsedService } from "src/app/help/color-used.service";
+import { ClipBoard } from "../../clipBoard.model";
 
 @Component({
-  selector: 'app-clipBoard-item',
-  templateUrl: './clipBoard-item.component.html',
-  styleUrls: ['./clipBoard-item.component.css'],
+  selector: "app-clipBoard-item",
+  templateUrl: "./clipBoard-item.component.html",
+  styleUrls: ["./clipBoard-item.component.css"],
 })
 export class ClipBoardItemComponent implements OnInit {
   @Input() clipBoard: ClipBoard;
@@ -19,19 +19,26 @@ export class ClipBoardItemComponent implements OnInit {
   blue: string = this.colorUsedService.blue;
   green: string = this.colorUsedService.green;
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    const clipBoardContent: string = this.clipBoard.content;
+    if (clipBoardContent.length > 350) {
+      this.isActiveScroll = true;
+      console.log("lenght" + clipBoardContent.length);
+    }
+  }
+
   onClickCopyToClipBoard(event: Event) {
-    // Get the text field
+    // Get the content field
     if (event != null) {
       console.log(event);
-      const copyText = this.clipBoard.text;
+      const copyContent = this.clipBoard.content;
 
-      // Copy the text inside the text field
-      navigator.clipboard.writeText(copyText);
+      // Copy the content inside the content field
+      navigator.clipboard.writeText(copyContent);
 
-      // Alert the copied text
-      //alert('Copied the text: ' + copyText);
-      console.log(copyText);
+      // Alert the copied content
+      //alert('Copied the content: ' + copycontent);
+      console.log(copyContent);
     }
   }
 
