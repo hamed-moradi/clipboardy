31e081aee7cc2781637852cfee22704e2fcad42a
Amir Hashemi <107415904+AmirH6977@users.noreply.github.com>
2023-09-05 09:39:21 +0330

    feat(Back):increase Device Name char
    feat(Back):increase Device Name char

--- src/Core/Core.Domain/Configurations/AccountDeviceConfiguration.cs
+++ src/Core/Core.Domain/Configurations/AccountDeviceConfiguration.cs
@@ -2,21 +2,27 @@
 using Core.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
-namespace Core.Domain.Configurations {
-  public class AccountDeviceConfiguration {
-    internal static void OnModelCreating(ModelBuilder modelBuilder) {
-      modelBuilder.Entity<Clipboard>()
-        .Property(p => p.status)
-        .HasDefaultValue(Status.ACTIVE);
+namespace Core.Domain.Configurations
+{
+    public class AccountDeviceConfiguration
+    {
+        internal static void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Clipboard>()
+              .Property(p => p.status)
+              .HasDefaultValue(Status.ACTIVE);
 
-      modelBuilder.Entity<AccountDevice>()
-        .HasOne(p => p.Account)
-        .WithMany()
-        .OnDelete(DeleteBehavior.Restrict);
+            modelBuilder.Entity<AccountDevice>()
+              .HasOne(p => p.Account)
+              .WithMany()
+              .OnDelete(DeleteBehavior.Restrict);
 
-      modelBuilder.Entity<AccountDevice>()
-        .Property(p => p.inserted_at)
-        .HasDefaultValueSql("now()");
+            modelBuilder.Entity<AccountDevice>()
+              .Property(p => p.inserted_at)
+              .HasDefaultValueSql("now()");
+
+            modelBuilder.Entity<AccountDevice>()
+                .Property(x => x.device_name).HasMaxLength(512);
+        }
     }
-  }
 }
---
+++ src/Core/Core.Domain/Migrations/20230905051734_increaseCharacterAccountName.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Core.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class increaseCharacterAccountName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "device_name",
+                table: "account_device",
+                type: "character varying(512)",
+                maxLength: 512,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(128)",
+                oldMaxLength: 128);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "device_name",
+                table: "account_device",
+                type: "character varying(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(512)",
+                oldMaxLength: 512);
+        }
+    }
+}
