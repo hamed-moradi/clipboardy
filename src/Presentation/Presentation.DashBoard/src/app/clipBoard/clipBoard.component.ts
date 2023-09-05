7bbde34821ee6928d677fcfcf18fb8e5427c7ada
AmirH6977 <107415904+AmirH6977@users.noreply.github.com>
2023-09-06 00:21:57 +0330

    feat(dashboard):fix extra call get request

--- src/Presentation/Presentation.DashBoard/src/app/clipBoard/clipBoard.component.ts
+++ src/Presentation/Presentation.DashBoard/src/app/clipBoard/clipBoard.component.ts
@@ -8,31 +8,31 @@ import {
   ViewChild,
   ElementRef,
   AfterViewInit,
-} from "@angular/core";
-import { NgForm } from "@angular/forms";
-import { fromEvent, map, timeout } from "rxjs";
+} from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { filter, fromEvent, map, takeWhile, tap, timeout } from 'rxjs';
 
-import { ClipBoardService } from "../shared/services/clipBoard.service";
-import { IClipBoard } from "./IClipBoard";
-import { ColorUsedService } from "../shared/services/color-used.service";
-import { MobileViewService } from "../shared/services/mobile-view.service";
-import { MatDialog } from "@angular/material/dialog";
-import { AddOrEditClipboardComponent } from "../shared/modals/addOrEditClipboard-modal/addOrEditClipboard-modal.component";
-import { Router } from "@angular/router";
-import { ErrorModalComponent } from "../shared/modals/error-modal/error-modal.component";
-import { NoopScrollStrategy } from "@angular/cdk/overlay";
+import { ClipBoardService } from '../shared/services/clipBoard.service';
+import { IClipBoard } from './IClipBoard';
+import { ColorUsedService } from '../shared/services/color-used.service';
+import { MobileViewService } from '../shared/services/mobile-view.service';
+import { MatDialog } from '@angular/material/dialog';
+import { AddOrEditClipboardComponent } from '../shared/modals/addOrEditClipboard-modal/addOrEditClipboard-modal.component';
+import { Router } from '@angular/router';
+import { ErrorModalComponent } from '../shared/modals/error-modal/error-modal.component';
+import { NoopScrollStrategy } from '@angular/cdk/overlay';
 
 @Component({
-  selector: "app-clipBoard",
-  templateUrl: "./clipBoard.component.html",
-  styleUrls: ["./clipBoard.component.scss"],
+  selector: 'app-clipBoard',
+  templateUrl: './clipBoard.component.html',
+  styleUrls: ['./clipBoard.component.scss'],
 })
 export class ClipBoardComponent implements OnInit, AfterViewInit {
   clipBoards: IClipBoard[];
 
   finished: boolean = true;
 
-  @ViewChild("newButtonClipBoard") myElementRef: ElementRef;
+  @ViewChild('newButtonClipBoard') myElementRef: ElementRef;
 
   constructor(
     public clipBoardService: ClipBoardService,
@@ -42,12 +42,12 @@ export class ClipBoardComponent implements OnInit, AfterViewInit {
     private errorDialog: MatDialog,
     private router: Router
   ) {
-    /*  this.clipBoardService
-      .getClipBoard(this.skip, this.take)
-      .pipe(map((get) => get.list))
-      .subscribe(
-        (getClipBoardResult) => (this.clipBoards = getClipBoardResult)
-      ); */
+    this.clipBoardService.getClipBoard(this.skip, this.take).pipe(
+      tap((get) => {
+        console.log(get);
+        this.totalCount = Math.ceil(get.totalCount);
+      })
+    );
   }
 
   violet: string = this.colorUsedService.violet;
@@ -59,6 +59,7 @@ export class ClipBoardComponent implements OnInit, AfterViewInit {
   isSearched: boolean = false;
   isLoading: boolean = false;
 
+  totalCount: number | null = null; // Initialize totalCount as null
   skip: number = 0;
   take: number = 0;
 
@@ -68,12 +69,12 @@ export class ClipBoardComponent implements OnInit, AfterViewInit {
   ngAfterViewInit(): void {
     if (window.innerWidth < 500) {
       const newButtonClipBoardElement = this.myElementRef.nativeElement;
-      this.mobileViewService.resizeEvent(newButtonClipBoardElement, "d-grid");
+      this.mobileViewService.resizeEvent(newButtonClipBoardElement, 'd-grid');
     }
-    fromEvent(window, "resize").subscribe(() => {
+    fromEvent(window, 'resize').subscribe(() => {
       if (window.innerWidth < 500) {
         const newButtonClipBoardElement = this.myElementRef.nativeElement;
-        this.mobileViewService.resizeEvent(newButtonClipBoardElement, "d-grid");
+        this.mobileViewService.resizeEvent(newButtonClipBoardElement, 'd-grid');
       }
     });
   }
@@ -107,7 +108,7 @@ export class ClipBoardComponent implements OnInit, AfterViewInit {
           // Show error dialog
           this.errorDialog.open(ErrorModalComponent, {
             data: {
-              message: "An error occurred during Add content to clipboard.",
+              message: 'An error occurred during Add content to clipboard.',
 
               error: errMes.error.title,
             },
@@ -135,7 +136,7 @@ export class ClipBoardComponent implements OnInit, AfterViewInit {
           // Show error dialog
           this.errorDialog.open(ErrorModalComponent, {
             data: {
-              message: "An error occurred during Edit content clipboard.",
+              message: 'An error occurred during Edit content clipboard.',
 
               error: errMes.error.title,
             },
@@ -161,7 +162,7 @@ export class ClipBoardComponent implements OnInit, AfterViewInit {
           // Show error dialog
           this.errorDialog.open(ErrorModalComponent, {
             data: {
-              message: "An error occurred during Delete content clipboard.",
+              message: 'An error occurred during Delete content clipboard.',
 
               error: errMes.error.title,
             },
@@ -174,7 +175,7 @@ export class ClipBoardComponent implements OnInit, AfterViewInit {
   onClipBoardSearchList(searchQuery: NgForm) {
     if (
       searchQuery.value.searchQuery === undefined ||
-      searchQuery.value.searchQuery === ""
+      searchQuery.value.searchQuery === ''
     ) {
       //console.log(this.clipBoards);
       return this.clipBoards;
@@ -190,26 +191,28 @@ export class ClipBoardComponent implements OnInit, AfterViewInit {
   }
 
   onScrollDown() {
-    var pageCount: number = 0;
+    if (!this.isLoading) {
+      this.isLoading = true;
+      this.take += 10;
 
-    this.take = this.take + 10;
-
-    this.isLoading = true;
-
-    this.clipBoardService
-      .getClipBoard(this.skip, this.take)
-      //.pipe(map((get) => get.list))
-      .subscribe({
-        next: (res) => (this.clipBoards = res.list),
-        complete: () => (this.isLoading = false),
-      });
-
-    /*     setTimeout(() => {
-      // Call the getClipBoard() method of your service
-      this.clipBoardService.getClipBoard(this.skip, this.take).subscribe({
-        next: (res) => (this.clipBoards = res.list),
-        complete: () => (this.isLoading = false), // Set the isLoading property to false to hide the spinner
-      });
-    }, 10000); */
+      if (this.totalCount === null || this.totalCount >= this.take) {
+        this.clipBoardService
+          .getClipBoard(this.skip, this.take)
+          .pipe(
+            tap((get) => {
+              console.log(get);
+              this.totalCount = Math.ceil(get.totalCount);
+            })
+          )
+          .subscribe({
+            next: (res) => {
+              this.clipBoards = res.list;
+              this.isLoading = false; // Move isLoading inside the next callback
+            },
+          });
+      } else {
+        this.isLoading = false; // Set isLoading to false if the condition is not met
+      }
+    }
   }
 }
