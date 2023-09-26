9550efab922f54e6438a8bb2981673d61829412c
Amir Hashemi <107415904+AmirH6977@users.noreply.github.com>
2023-09-26 10:51:06 +0330

    feat(dashboard):Upgrade to Angular 16 -1

--- src/Presentation/Presentation.DashBoard/src/app/auth/auth.guard.ts
+++ src/Presentation/Presentation.DashBoard/src/app/auth/auth.guard.ts
@@ -1,17 +1,12 @@
 import { Injectable } from "@angular/core";
-import {
-  ActivatedRouteSnapshot,
-  CanActivate,
-  Router,
-  RouterStateSnapshot,
-} from "@angular/router";
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot } from "@angular/router";
 import { Observable } from "rxjs";
 import { map, take, tap } from "rxjs/operators";
 
 import { AuthService } from "../shared/services/auth.service";
 
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
   constructor(private authService: AuthService, private router: Router) {}
 
   canActivate(
